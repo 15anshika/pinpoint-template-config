6ad877dd18609fa71cccf3c2cc1e50f9fd25d830
devops2615 <geetikajaggi777@gmail.com>
2022-10-29 00:31:55 -0400

    .

--- template_active_version_update.sh
+++ template_active_version_update.sh
@@ -8,7 +8,7 @@ function update_version(){
 	read
 	while IFS=, read -r Mode Template Version
 	do
-		#echo "Mode $Mode , Template $Template , Version $Version"
+		echo "Mode $Mode , Template $Template , Version $Version"
 		echo "aws pinpoint update-template-active-version --template-active-version-request Version=$Version --template-name $Template --template-type $Mode"
 				if [ $? -eq 0 ]; then
 						echo "Sucesssful: Active version for $Mode Template: '$Template' updated to: $Version"
